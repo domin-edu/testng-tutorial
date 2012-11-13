482d061cb323c55fdb51f9aaec99ef9e7643e95e
Domin Edu <domin-edu@server>
2012-11-13 18:10:13 +0100

    rozbudowa testu wykorzystyjacego adnotacje przygotowujace i sprzatajace po testach

--- testng/src/test/java/pl/grinder/tutorial/testng/AdnotacjeMetodTest.java
+++ testng/src/test/java/pl/grinder/tutorial/testng/AdnotacjeMetodTest.java
@@ -4,11 +4,14 @@ import static junit.framework.Assert.fail;
 import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterGroups;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Test(sequential=true)
 public class AdnotacjeMetodTest {
 
 	@BeforeClass
@@ -16,9 +19,29 @@ public class AdnotacjeMetodTest {
 		System.out.println("<< Przed wszystkimi testami >>");
 	}
 
+	@BeforeGroups("pozytywne")
+	private void przedGrupaTestowPozytywnych() {
+		System.out.println("<< Przed wszystkimi testami z grupy pozytywnych >>");
+	}
+	
+	@BeforeGroups("negatywne")
+	private void przedGrupaTestowNegatywnych() {
+		System.out.println("<< Przed wszystkimi testami z grupy negatywnych >>");
+	}
+	
 	@BeforeMethod
 	private void przedKazdaMetodaTestowa() {
 		System.out.println("<< Przed metoda testowa >>");
+	}
+	
+	@AfterGroups("pozytywne")
+	private void podGrupieTestowPozytywnych() {
+		System.out.println("<< Po wszystkimi testach z grupy pozytywnych >>");
+	}
+	
+	@AfterGroups("negatywne")
+	private void ppoGrupieTestowNegatywnych() {
+		System.out.println("<< Po wszystkich testach z grupy negatywnych >>");
 	}
 
 	@AfterMethod
@@ -31,13 +54,23 @@ public class AdnotacjeMetodTest {
 		System.out.println("<< Po wszystkich testach >>");
 	}
 
-	@Test
-	public void pierwszyTest() {
+	@Test(groups="pozytywne")
+	public void pozytywnyTest1() {
 		assertTrue(true, "true musi byc prawda");
 	}
 	
-	@Test
-	public void negatywnyTest() {
+	@Test(groups="pozytywne")
+	public void pozytywnyTest2() {
+		assertTrue(true, "true musi byc prawda");
+	}
+	
+	@Test(groups="negatywne")
+	public void negatywnyTest1() {
+		fail("Nie udalo sie...");
+	}
+	
+	@Test(groups="negatywne")
+	public void negatywnyTest2() {
 		fail("Nie udalo sie...");
 	}
 }
