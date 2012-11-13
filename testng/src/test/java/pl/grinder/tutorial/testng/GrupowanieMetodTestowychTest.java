ef152e48bbc5fc4458e4d2750e00083be91c85e6
Domin Edu <domin-edu@server>
2012-11-13 18:19:25 +0100

    wydzielenie metody do usypiania

--- testng/src/test/java/pl/grinder/tutorial/testng/GrupowanieMetodTestowychTest.java
+++ testng/src/test/java/pl/grinder/tutorial/testng/GrupowanieMetodTestowychTest.java
@@ -9,23 +9,28 @@ import org.testng.annotations.Test;
 public class GrupowanieMetodTestowychTest {
 	@Test(groups="wolne") 
 	public void wolny_test_1() throws InterruptedException {
-		Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+		uspijNa(1);
 		
 		assertTrue(1 < 2);
 	}
+
 	
 	@Test(groups="wolne") 
 	public void wolny_test_2() throws InterruptedException {
-		Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+		uspijNa(2);
 		
 		assertTrue(1 < 2);
 	}
 	
 	@Test(groups="wolne") 
 	public void wolny_test_3() throws InterruptedException {
-		Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+		uspijNa(3);
 		
 		assertTrue(1 < 2);
+	}
+
+	private void uspijNa(int liczbaSekund) throws InterruptedException {
+		Thread.sleep(TimeUnit.SECONDS.toMillis(liczbaSekund));
 	}
 	
 	@Test(groups="szybkie") 
